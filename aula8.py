e80ee3e285f91d2bdf29df84bc1e289ab9ee25c8
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-03-03 19:35:02 -0300

    Exercício com variáveis e tipos de dados

---
+++ aula8.py
@@ -0,0 +1,12 @@
+nome = 'Nome'
+Sobrenome = 'Sobrenome'
+idade = 19
+ano_nascimento = 2004
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+print('Nome:', nome)
+print('Sobrenome:', Sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
