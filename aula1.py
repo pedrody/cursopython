f7ee24df042f6b84ca1d65e5551b1ddabd5c910f
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-03-03 11:40:51 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,9 @@
+"""
+- DocString
+
+Posso usar para anotações.
+"""
+
+# Comentário
+print(123)  # Comentário
+print(456)
---
+++ aula2.py
@@ -0,0 +1,6 @@
+# \r\n -> CRLF
+# \n -> LF
+
+print(12, 34, sep="", end='#')
+print(56, 78, sep='-', end='\r\n')
+print(9, 10, sep='-', end='\n')
