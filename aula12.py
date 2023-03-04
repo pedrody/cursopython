03c6673fd188d294659ceafb480498a65e820a70
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-03-04 14:44:16 -0300

    Cálculo IMC
    Solução exercício de programação - Cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,9 @@
+# calculado dividindo o peso (em kg) pela altura ao quadrado (em metros).
+nome = 'Nome'
+altura_m = 1.80
+peso_kg = 95
+imc = peso_kg / altura_m ** 2
+
+print(nome, 'tem', altura_m, 'm de altura,')
+print('pesa', peso_kg, 'kg e seu IMC é',)
+print(imc)
