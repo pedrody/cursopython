be79b18015e5d80e3a22785b438341556c97960d
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-03-05 19:40:57 -0300

    if / elif / else (fluxo do interpretador)
    if, elif e else: entendendo o fluxo do interpretador em condicionais

---
+++ aula17.py
@@ -0,0 +1,16 @@
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('Condição 1')
+elif condicao2:
+    print('Condição 2')
+elif condicao3:
+    print('Condição 3')
+elif condicao4:
+    print('Condição 4')
+else:
+    print('Nenhuma condição')
+print('Fora do if')
