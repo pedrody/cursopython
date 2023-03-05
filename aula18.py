b8e0495103a4621dfce417f9dd48b8bccdcc3c93
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-03-05 19:41:43 -0300

    Debugger
    O Debugger do VS Code e o interpretador do Python lendo os códigos

---
+++ aula18.py
@@ -0,0 +1,20 @@
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Condição 1')
+elif condicao2:
+    print('Condição 2')
+elif condicao3:
+    print('Condição 3')
+elif condicao4:
+    print('Condição 4')
+else:
+    print('Nenhuma condição')
+
+if 1 == 1:
+    print('Outro if')
+
+print('Fora do if')
