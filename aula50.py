66952fa697b167889492019a1f9922797f70c7d5
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2023-03-11 17:29:18 -0300

    Exercício - exiba os índices da lista

---
+++ aula50.py
@@ -0,0 +1,18 @@
+# Exercício
+# Exiba os índices da lista 
+
+# Minha solução.
+lista = ['Maria', 'Helena', 'Luiz', 'Pedro']
+i = 0
+
+for nome in lista:
+    print(i, '-',nome)
+    i += 1
+
+# Solução professor.
+# lista = ['Maria', 'Helena', 'Luiz', 'Pedro']
+# lista.append('Exemplo')
+# indices = range(len(lista))
+
+# for indice in indices:
+#     print(indice, lista[indice], type(lista[indice]))
